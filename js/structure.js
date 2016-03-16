8c8e89dfd6847b3c36053d69d5030376a4c17f13
acttechie55 <actaylor55@gmail.com>
2016-03-15 22:05:58 -0500

    Added "This Website" to Recent Works

--- js/structure.js
+++ js/structure.js
@@ -1,39 +1,40 @@
-$(document).ready(function () {
-	var section = $('.section');
-	$(".section-content-diff").hide();
-	$(".skills-bar-graph").hide();
-	$(".recent-work-section").hide();
-	$(".home .section-title").hide();
-	$(".section-content-edu").hide();
-	$(".social ul li i").css("border-color", "#ffd700");
-	
+$(document).ready(function() {
+    var section = $('.section');
+    $(".section-content-diff").hide();
+    $(".skills-bar-graph").hide();
+    $(".recent-work-section").hide();
+    $(".home .section-title").hide();
+    $(".section-content-edu").hide();
+    $(".social ul li i").css("border-color", "#ffd700");
+
 	/*Clone Scripts
 	========================*/
-	
-	/*Home Slide - DO NOT CHANGE */
-	$('#site-title').clone().appendTo('.home-section').css({"border": "1.3em solid #ffd700", "padding": "2em"});
-	/*About Slide - DO NOT CHANGE*/
-	$('.first-about-resp, .second-about-resp').clone().appendTo('.about-section').css({"color": "#222222", "text-align":"center", "padding":"2em", "padding-bottom":".5em",  "padding-top":".5em", "margin-bottom":"0em", });
-	
-	/*Skills Slide - DO NOT CHANGE*/
-	$('.skills-bar-graph').clone().appendTo('.skills-section').addClass('inner').show();
 
-	/*Recent Works Slide - DO NOT CHANGE*/
-	$('.plat-blog-content').clone().appendTo('.1st-content');
-	$('.inc-content').clone().appendTo('.2nd-content');
-	$('.grit-content').clone().appendTo('.3rd-content');
-	$('.refill-content').clone().appendTo('.4th-content');	
-    $('.lawn-content').clone().appendTo('.5th-content');	
-	
-	/*Education Slide - DO NOT CHANGE*/
-	$('.edu-resp').clone().appendTo('.education-section').css({ "position": "absolute", "right": "2em"}).addClass('inner');
-	
+    /*Home Slide - DO NOT CHANGE */
+    $('#site-title').clone().appendTo('.home-section').css({ "border": "1.3em solid #ffd700", "padding": "2em" });
+    /*About Slide - DO NOT CHANGE*/
+    $('.first-about-resp, .second-about-resp').clone().appendTo('.about-section').css({ "color": "#222222", "text-align": "center", "padding": "2em", "padding-bottom": ".5em", "padding-top": ".5em", "margin-bottom": "0em", });
 
-function toggleAccordion() {
-		section.removeClass('active');
-		$(this).addClass('active');
-	}
-	section.click(toggleAccordion);
+    /*Skills Slide - DO NOT CHANGE*/
+    $('.skills-bar-graph').clone().appendTo('.skills-section').addClass('inner').show();
 
-	
+    /*Recent Works Slide - DO NOT CHANGE*/
+    $('.plat-blog-content').clone().appendTo('.1st-content');
+    $('.inc-content').clone().appendTo('.2nd-content');
+    $('.grit-content').clone().appendTo('.3rd-content');
+    $('.refill-content').clone().appendTo('.4th-content');
+    $('.madeby-content').clone().appendTo('.5th-content');
+    $('.lawn-content').clone().appendTo('.6th-content');
+
+    /*Education Slide - DO NOT CHANGE*/
+    $('.edu-resp').clone().appendTo('.education-section').css({ "position": "absolute", "right": "2em" }).addClass('inner');
+
+
+    function toggleAccordion() {
+        section.removeClass('active');
+        $(this).addClass('active');
+    }
+    section.click(toggleAccordion);
+
+
 });
